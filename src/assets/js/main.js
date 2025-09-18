45d16b02f4889a703002458555a5de49da49a196
Luca Milone <lucamilone96@gmail.com>
2025-09-18 18:34:57 +0200

    Rimosso il preloader e aggiornato il titolo della sezione catalogo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,17 +4,6 @@ Author: BiBi Chod Rani
 */
 
 (function () {
-  //===== Prealoder
-
-  window.onload = function () {
-    window.setTimeout(fadeout, 500);
-  };
-
-  function fadeout() {
-    document.querySelector(".preloader").style.opacity = "0";
-    document.querySelector(".preloader").style.display = "none";
-  }
-
   /*=====================================
     Sticky
     ======================================= */
